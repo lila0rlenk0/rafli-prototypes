cbf06af3752759ad3542269874bfc68b6cff3380
Wilson Macedo <wil.macedo.sa@gmail.com>
2026-01-10 18:32:13 -0300

    feat: :sparkles: added storage media url

--- next.config.ts
+++ next.config.ts
@@ -10,16 +10,7 @@ const nextConfig: NextConfig = {
 	images: {
 		// Allow loading images from local network (fixes private IP error)
 		unoptimized: process.env.NODE_ENV === 'development',
-		remotePatterns:
-			process.env.NODE_ENV === 'development'
-				? [
-						{
-							protocol: 'http',
-							hostname: '127.0.0.1',
-							port: '9800',
-						},
-					]
-				: [new URL(env.APP_URL)],
+		remotePatterns: [new URL(env.STORAGE_MEDIA_URL)],
 	},
 	cacheComponents: true,
 };
--- src/env/server.ts
+++ src/env/server.ts
@@ -7,11 +7,13 @@ import z from 'zod';
  */
 export const env = createEnv({
 	server: {
-		BACKEND_URL: z.url(),
+		BACKEND_URL: z.url().default('http://localhost:4000'),
 		APP_URL: z.url().default('http://localhost:3000'),
+		STORAGE_MEDIA_URL: z.string().default('http://127.0.0.1:9800'),
 	},
 	runtimeEnv: {
 		BACKEND_URL: process.env.BACKEND_URL,
 		APP_URL: process.env.APP_URL || process.env.NEXT_PUBLIC_APP_URL,
+		STORAGE_MEDIA_URL: process.env.STORAGE_MEDIA_URL,
 	},
 });

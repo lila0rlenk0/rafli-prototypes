7be594643bf570fc2fa613af7ed600dab9da9245
Wilson Macedo <wil.macedo.sa@gmail.com>
2026-01-20 10:28:07 -0300

    feat: :sparkles: added new error page

--- src/app/(protected)/my-raffles/page.tsx
+++ src/app/(protected)/my-raffles/page.tsx
@@ -7,6 +7,8 @@ import { StatusTabs } from '@/app/(protected)/my-raffles/status-tabs';
 import { getMyRaffles } from '@/services/raffle/get-my-raffles';
 import { RAFFLE_STATUS } from '@/types/raffle';
 import { CreateRaffleButton } from './create-raffle-button';
+import { BugIcon } from '@/assets/icons/bug-icon';
+import Link from 'next/link';
 
 interface PageProps {
 	searchParams: Promise<{
@@ -35,13 +37,22 @@ export default async function MyRafflesPage({ searchParams }: PageProps) {
 
 	if (!response.success) {
 		return (
-			<div className="flex h-[50vh] w-full items-center justify-center">
-				<div className="text-center">
-					<h3 className="text-lg font-medium text-red-600">
-						Error loading raffles
-					</h3>
-					<p className="mt-2 text-gray-500">Failed to load raffles</p>
-				</div>
+			<div className="flex h-[50vh] w-full flex-col items-center justify-center gap-10 text-center">
+				<BugIcon />
+
+				<hgroup className="space-y-4">
+					<h2 className="text-xl font-semibold">Error loading your raffles</h2>
+					<p className="mt-2 text-lg">
+						Something went wrong while trying to load your raffles.
+					</p>
+				</hgroup>
+
+				<Link
+					href="/my-raffles"
+					className="rounded-full border border-black px-12 py-3 text-sm font-semibold text-black transition-colors"
+				>
+					Raffles
+				</Link>
 			</div>
 		);
 	}
